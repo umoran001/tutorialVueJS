ed4a08cd75fb52449fd0bc5e3372a12cea57b662
umoran001 <umoran001@ikasle.ehu.eus>
2024-01-30 23:38:09 +0100

    fondo beltza

--- FirstVueProyect/src/App.vue
+++ FirstVueProyect/src/App.vue
@@ -1,70 +1,40 @@
 <template>
   <div id="app">
-    <header>
-      <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            <img src="/src/images/logo.png" alt="" width="30" height="24">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasnavbar"
-            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">FirstVueProyect</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link mx-lg-2" href="#">Product</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link mx-lg-2" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link mx-lg-2" href="#">Community</a>
-                </li>
-              </ul>
-              <li class="nav-button">
-                <a class="btn btn-outline-primary mx-lg-2" href="#">LaunchPad</a>
-              </li>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
-
-    <main class="container-fluid py-4">
-      <router-view /> <!-- Componente para mostrar las vistas de las diferentes páginas -->
-    </main>
-
-
-    <footer class="bg-light text-dark pt-5 pb-4">
-      <div class="container text-center twxt-md-start">
-        <div class="row text-center text-md-start">
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h5 class="text-uppercase mb-4 font-weight-bold">FirstVueProyect</h5>
-            <p>FirstVueProyect es una aplicación web desarrollada con Vue.js.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <Header />
+    <Intro />
+    <Arbitrum />
+    <LevelUp />
+    <Roadmap />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Menu from './components/Menu.vue'; // Importa el componente de menú
+import Header from './components/Header.vue';
+import Intro from './components/Intro.vue';
+import Arbitrum from './components/Arbitrum.vue';
+import LevelUp from './components/LevelUp.vue';
+import Roadmap from './components/Roadmap.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
-    Menu // Registra el componente de menú
-  },
-  // Otras opciones y lógica de la aplicación
+    Header,
+    Intro,
+    Arbitrum,
+    LevelUp,
+    Roadmap,
+    Footer
+  }
 }
 </script>
 
 <style>
-/* Estilos generales de la aplicación */
+/* Estilos globales de la aplicación */
+body, html {
+  /* Establece la altura del body y html al 100% */
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 </style>
---
+++ FirstVueProyect/src/components/Intro.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="intro">
+      <div class="background-image"></div>
+      <!-- Contenido del componente Intro -->
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    // Lógica del componente Intro
+  }
+  </script>
+  
+  <style scoped>
+  .intro {
+    /* Estilos para el contenedor principal */
+    position: relative;
+    width: 100%;
+    /* Establece una altura específica para el div de Intro */
+    height: 400px; /* Por ejemplo, establece una altura de 400 píxeles */
+  }
+  
+  .background-image {
+    /* Estilos para el contenedor de la imagen de fondo */
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('../images/cabecera.png');
+    background-size: cover;
+    background-position: center;
+    z-index: 1;
+  }
+  
+  .intro::before {
+    /* Estilos para el fondo negro */
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.11); /* Reducir la opacidad al 11% */
+    z-index: 0;
+  }
+  </style>
+  
